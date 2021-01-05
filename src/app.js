4a6aa1fe1061e479d1197fe3d477286fc7bbb792
ms-brahim <brahim7khalil@gmail.com>
2021-01-05 17:47:20 +0100

    EDIT Data Product

--- src/app.js
+++ src/app.js
@@ -5,12 +5,14 @@ const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-// mysqlConnection 
+// mySQL Connection 
 var mysqlConnection = mysql.createConnection({
     host:'localhost',
     user:'root',
     password:'',
-    database:'prod_categ_db'
+    database:'prod_categ_db',
+    debug: false,
+    multipleStatements: true
 });
 
 mysqlConnection.connect((err)=>{
@@ -33,14 +35,18 @@ app.get('/', (req,res)=>{
     res.render('index');
 });
 
+
 //////////////// PRODUCTS DATA ///////////////////
 // dispaly All Items (products) 
 app.get('/product', (req,res)=>{
-    let sql = 'SELECT products.*, categories.name FROM products INNER JOIN categories WHERE products.category_id = categories.id';
-    mysqlConnection.query(sql,(err,rows)=>{
+    //let sql1 = 'SELECT * FROM products categories';
+    let sql = 'SELECT * FROM categories;SELECT * FROM products P , categories C WHERE P.category_id = C.id';
+    mysqlConnection.query(sql, [1, 2],(err,rows)=>{
         if (!err) {
+            //console.log(rows[1])
             res.render('product',{
-                products:rows,
+                products:rows[1],
+                categories:rows[0]
             });
         }else{
             console.log(err);
@@ -55,7 +61,7 @@ app.get('/add', (req, res) => {
 });  
 
 app.post('/save', (req, res) => {
-	let data = {name: req.body.name, price: req.body.price, category_id: req.body.category_id};
+	let data = {nameP: req.body.name, price: req.body.price, category_id: req.body.category_id};
     let sql = "INSERT INTO products SET ?"
     let query = mysqlConnection.query(sql,data,(err, results)=>{
         if(err) throw err;
@@ -64,9 +70,28 @@ app.post('/save', (req, res) => {
 });
 
 
+// EDIT Item (products) 
+app.get('/product/edit/:id',(req,res)=>{
+    const Uid = req.params.id;
+    let sql = 'SELECT * FROM products P , categories C WHERE P.category_id = C.id WHERE Pid ='.Uid;
+    mysqlConnection.query(sql,(err,result)=>{
+        if(!err)
+        console.log(result[1]),
+        res.redirect('/product',{
+           productEdit:result[0]
+           
+        });
+        else
+        console.log(err);
+        
+        
+    })
+})
+
+
 // Delete Item (products) to Table
 app.get('/product/remove/:id',(req,res)=>{
-    let sql = 'DELETE FROM products WHERE id = ?';
+    let sql = 'DELETE FROM products WHERE Pid = ?';
     mysqlConnection.query(sql,[req.params.id],(err)=>{
         if(!err)
         res.redirect('/product');
@@ -77,18 +102,6 @@ app.get('/product/remove/:id',(req,res)=>{
 
 
 //////////////// CATEGORIES DATA ///////////////////
-app.get('/product', (req,res)=>{
-    let sql = 'SELECT * FROM categories';
-    mysqlConnection.query(sql,(err,rows)=>{
-        if (!err) {
-            res.render('product',{
-                categories:rows,
-            });
-        }else{
-            console.log(err);
-        }
-    })
-});
 
 
 app.listen(port, () => console.log(`listening on port ${port}`));
